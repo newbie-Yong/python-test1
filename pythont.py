617d2d98de10288bb106ceef2c2d3fdebbb98376
newbie-Yong <75254434+newbie-Yong@users.noreply.github.com>
2020-12-02 12:32:06 +0900

    Add files via upload

---
+++ pythont.py
@@ -0,0 +1,99 @@
+# 1번
+
+국어,영어,수학 =80,75,55
+(국어+영어+수학)/3
+
+
+# 2번
+
+ a = 13%2
+ 
+ if a>0:
+     print("홀수")
+ else:
+     print("짝수")
+
+홀수
+
+
+#3번
+
+pin = "881120-1068234"
+yyyymmdd = pin[:6]
+num = pin[7:]
+print(yyyymmdd)
+881120
+print(num)
+1068234
+
+#4번
+
+pin = "881120-1068234"
+print(pin[7])
+
+#5번
+
+a = "a:b:c:d"
+b = a.replace(":","#")
+print(b)
+
+
+#6번
+
+
+a = [1,3,5,4,2]
+a.sort()
+a.reverse()
+print(a)
+
+
+
+#7
+
+a = ['life','is','too','short']
+" ".join(a)
+
+"{0} {1} {2} {3}".format('life', 'is', 'too', 'short')
+
+
+#8번
+
+a = (1,2,3)
+a = (1,2,3) + (4,)
+print(a)
+
+#9번
+
+'''
+
+2번.? 쉼표쓰면 오류나니깐
+3번 같은 경우에는..음..첫번째가 지정될 것 같음
+
+'''
+
+#10번
+
+a = {'a':90,'b':80,'c':70}
+result = a['b']
+print(a)
+print(result)
+
+
+#11번
+
+a = [1,1,1,2,2,3,3,3,4,4,5]
+aset = set(a)
+b = list(aset)
+print(b)
+
+#12번
+
+'''
+b값은 바뀐다.
+왜냐
+
+
+
+
+
+
